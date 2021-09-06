0f9046ef341574f5d356578c2c7c6e8224f271c2
unknown <asfyd42@gmail.com>
2021-09-06 10:41:02 -0300

    Update 1.1

--- src/components/Messagepage/Sidebarchat.js
+++ src/components/Messagepage/Sidebarchat.js
@@ -1,16 +1,43 @@
 import { Avatar } from '@material-ui/core'
-import React from 'react'
+import React, {useState ,useEffect} from 'react'
 
-export default function Sidebarchat() {
-    return (
+export default function Sidebarchat( { addNewChat } ) {
+
+     const [seed, setSeed] = useState('')
+
+     useEffect( () => {
+
+       Math.floor(
+           Math.random() * 5000)
+
+     },  [])
+
+     const createChat = () => {
+         const roomName =prompt("please enter name for chat");
+
+         if (roomName) {
+             //do some database staff
+             console.log(roomName)
+         }
+     };
+
+
+    return !addNewChat ? (
         <div className = "sidebarChat">
+              
 
-           <Avatar />
+           <Avatar src= "https://avatars.dicebear.com/api/human/1.svg" />
+
            <div className = "sidebarChat__info">
-               <h2>Room Name</h2>
+               <h4>Room Name</h4>
                <p>Last message.....</p>
-
            </div>
+
+
+        </div>
+    ): (
+        <div onClick={createChat} className = "sidebarChat">
+             <h2>Add new chat</h2>
         </div>
     )
 }
